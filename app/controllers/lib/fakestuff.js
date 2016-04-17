b2ae56cd8b083b9beed614acfc300d7be70140fb
Raheela Khan <raheelak20@gmail.com>
2016-04-16 20:17:23 -0700

    never make http request when saving doc to mongo 0_0

--- app/controllers/lib/fakestuff.js
+++ app/controllers/lib/fakestuff.js
@@ -26,12 +26,57 @@ function getBook(cb) {
             });
             bookArr.shift();
             return cb(bookArr);
+            
         });
        
     }).on('error', (e) => {
         console.log(e);
     });
 };
+
+function getBookImage(bookArr,cb) {
+
+        var ind = bookArr.lastIndexOf('by');
+        
+        var bookData = {
+            title: bookArr.substr(0,ind),
+            author:bookArr.substr(ind+2)
+        };
+        
+
+         var url = 'https://www.googleapis.com/books/v1/volumes?q='+bookData.title.toLowerCase()+'+inauthor:'+bookData.author.toLowerCase()+'&key='+config.apiKey ;
+        
+        return cb(url);
+
+}
+
+function makeRequest(url,cb) {
+    var data = '';
+    https.get(url, (res) => {
+                            console.log('statuscode', res.statusCode);
+                            
+                            res.on('data', (d) => {
+                                data += d;
+                            });
+                            
+                            
+                            res.on('end' , () => {
+                               return cb(data);
+                             });
+                        }).on('error', (e) => {
+                            console.error('error in book request',e);
+                        }); // end inner http request
+}
+
+function handleIt(data,cb) {
+    data = JSON.parse(data);
+    
+    if (data.totalItems > 0) {
+        index += 1;
+        return cb(data.items[0].volumeInfo.imageLinks.thumbnail);
+    }
+  
+}
 
 function read(cb) {
     
@@ -67,7 +112,7 @@ function saveUser(user,book) {
     });
 }
 
-function handleIt(bookData,data,user) {
+/*function handleIt(bookData,data,user) {
     data = JSON.parse(data);
     
     if (data.totalItems > 0) {
@@ -84,9 +129,9 @@ function handleIt(bookData,data,user) {
             }
         });
     }
-}
+}*/
 
-function getBookImage(url, bookData,user) {
+/*function getBookImage(url, bookData,user) {
     var data = '';
     https.get(url, (res) => {
                             console.log('statuscode', res.statusCode);
@@ -102,7 +147,7 @@ function getBookImage(url, bookData,user) {
                         }).on('error', (e) => {
                             console.error('error in book request',e);
                         }); // end inner http request
-}
+}*/
 
 function saveBook(user,book,i) {
     console.log('now its my turn');
@@ -145,16 +190,27 @@ function makeUser(name,book,i) {
 //get a name for user and book/
 exports.getData = function() {
     var k = 0;
-    
+    var bookImageArr = [];
         read(function(name) {
             name = name.slice(0,10);
         
-             getBook(function(book) {
-                book = book.slice(0,10);
-                
-                console.log(book,name);
+             getBook(function(book) {    
+                console.log('hi',book,name);
+                    for (var i = 0; i < 10; i++) {
+                        getBookImage(book[i], function(url) {
+                            makeRequest(url,function(data) {
+                                handleIt(data,function(bookImage) {
+                                    bookImageArr.push(bookImage);
+                                    console.log(index,'index');
+                                    if (index === 10) {
+                                        console.log(bookImageArr);
+                                    }
+                                });
+                            });
+                        })
+                    };
                  
-                for (var i = 0; i < name.length; i++ ) {
+             //   for (var i = 0; i < name.length; i++ ) {
                     
                /*     makeUser(name,book,i);
                     var userData = {
@@ -165,11 +221,11 @@ exports.getData = function() {
                         provider:'local'
                     } */
                     
-                    console.log('index and i ', i, index);
+            /*        console.log('index and i ', i, index);
                     if (index === i) {
                         console.log('so I happen');
                         makeUser(name,book,i);
-                    }
+                    }*/
                 /*    console.log(user,'the User');
                     user.save(function(err) {
                         console.log('error',err,'the user', user);
@@ -210,8 +266,8 @@ exports.getData = function() {
                     }); //end user save */
                     
                     
-                } //end for loop
-                 
+             //   } //end for loop
+           
             }); //end get book function call
         }); //end get names functionc call
     
