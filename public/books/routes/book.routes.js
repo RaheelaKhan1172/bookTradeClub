88a3ccbe7b627ec62bfa11323e121decc4b987c6
Raheela Khan <raheelak20@gmail.com>
2016-04-29 23:57:00 -0700

    fixed views a bit fixed issue #1

--- public/books/routes/book.routes.js
+++ public/books/routes/book.routes.js
@@ -1,9 +1,11 @@
 angular.module('books').config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/books/:id', {
-        templateUrl: 'books/views/view-book.html'
+        templateUrl: 'books/views/view-book.html',
+        authenticate:true
     })
     .when('/books', {
-        templateUrl: 'books/views/all-books.html'
+        templateUrl: 'books/views/all-books.html',
+        authenticate:true
     })
     .otherwise({redirectTo: 'books/views/all-books.html'});
 }]);
